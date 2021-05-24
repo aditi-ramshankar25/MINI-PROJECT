1755cc9de0f3a1d2c9e30c093b493e38c93bc477
aditi-ramshankar25 <84516851+aditi-ramshankar25@users.noreply.github.com>
2021-05-24 21:39:55 +0530

    Create data of images.py

---
+++ data of images.py
@@ -0,0 +1,3 @@
+downloader.download("cute puppy",limit=30,output_dir='images',adult_filter_off=True)
+downloader.download("pretty sunflower",limit=30,output_dir='images',adult_filter_off=True)
+downloader.download("ice cream cone",limit=30,output_dir='images',adult_filter_off=True)
